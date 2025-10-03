f19ea8debfbabe31041f5a65438a4a3f7114404b
Jannatul Mahia <jannatulmahia8215@gmail.com>
2025-10-04 04:16:59 +0600

    updated settings

--- household/settings.py
+++ household/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
 
     # Local
     "core",
-    "payments",
+    # "payments",
 ]
 
 
--- household/urls.py
+++ household/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')), 
-    path("api/", include("payments.urls")),
+    path("api/", include("core.payments.urls")),
+
  
 ]
